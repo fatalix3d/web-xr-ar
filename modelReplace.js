b95298c1c47b2b54cfb1facea27b5cd5a2f3b1f4
fatalix3d <luckytima@gmail.com>
2023-06-12 17:44:04 +0600

    button add 6

--- modelReplace.js
+++ modelReplace.js
@@ -3,26 +3,46 @@ document.addEventListener('DOMContentLoaded', function() {
     // Получите ссылку на <model-viewer> элемент
     const viewer = document.querySelector('model-viewer');
 
-    // Слушайте событие размещения модели в режиме AR
+    // Обработчик события загрузки модели
     viewer.addEventListener('model-visibility', function(event) {
         // Проверьте, что модель видна в режиме AR
         if (event.detail.visible) {
-            // Запустите анимацию модели
-            viewer.play();
-        } else {
-            // При скрытии модели остановите анимацию (если нужно)
-            viewer.pause();
+            // Получите ссылку на сцену AR
+            const arScene = viewer.scene;
+
+            // Создайте переменные для хранения начальных координат пальца
+            let startX = 0;
+            let startY = 0;
+
+            // Слушайте событие касания экрана (touchstart)
+            arScene.addEventListener('touchstart', function(event) {
+                // Получите координаты касания пальца
+                const touch = event.touches[0];
+                startX = touch.clientX;
+                startY = touch.clientY;
+            });
+
+            // Слушайте событие перемещения пальца (touchmove)
+            arScene.addEventListener('touchmove', function(event) {
+                // Получите текущие координаты пальца
+                const touch = event.touches[0];
+                const currentX = touch.clientX;
+                const currentY = touch.clientY;
+
+                // Вычислите смещение пальца по осям X и Y
+                const offsetX = currentX - startX;
+                const offsetY = currentY - startY;
+
+                // Переместите модель на смещение пальца
+                viewer.cameraOrbit += offsetX * 0.01;
+                viewer.cameraTargetX += offsetY * 0.01;
+
+                // Обновите начальные координаты пальца
+                startX = currentX;
+                startY = currentY;
+            });
         }
     });
 
-    // Получите ссылку на кнопку для замены модели
-    const replaceButton = document.getElementById('replace-button');
-
-    // Слушайте событие клика на кнопке
-    replaceButton.addEventListener('click', function() {
-        // Измените источник модели
-        viewer.src = 'box_1m.glb';
-        // Остановите анимацию (если нужно)
-        viewer.pause();
-    });
+    // ... Другой код обработки событий и логика
 });
